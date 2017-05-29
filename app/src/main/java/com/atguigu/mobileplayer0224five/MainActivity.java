bcc6626ae032273dd0c5340ef794726affc35fd6
tianxuewei1016 <93226539@qq.com>
2017-05-29 14:07:10 +0800

    增加一个退出的功能

--- app/src/main/java/com/atguigu/mobileplayer0224five/MainActivity.java
+++ app/src/main/java/com/atguigu/mobileplayer0224five/MainActivity.java
@@ -5,10 +5,13 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.atguigu.mobileplayer0224five.base.BaseFragment;
 import com.atguigu.mobileplayer0224five.fragment.LocalAudioFragment;
@@ -31,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int position;
     private Fragment tempFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -99,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
             tempFragment = currentFragment;
         }
     }
+
     private void initFragment() {
         fragments = new ArrayList<>();
         fragments.add(new LocalVideoFragment());//本地视频
@@ -129,5 +134,26 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    private boolean isExit = false;
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (position != 0) {
+                rgMain.check(R.id.rb_local_video);
+                return true;
+            } else if (!isExit) {
+                Toast.makeText(MainActivity.this, "再按一次退出软件", Toast.LENGTH_SHORT).show();
+                isExit = true;
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        isExit = false;
+                    }
+                }, 2000);
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
